7cdba79f3f7116c62aecd19a6623ec69dd4ea324
Simon Vargas <sjvargas01@gmail.com>
2021-12-20 23:01:16 -0500

    first commit

---
+++ src/main/java/com/example/roomwebappmain/RoomWebAppMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomwebappmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWebAppMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomWebAppMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/roomwebappmain/RoomWebAppMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roomwebappmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomWebAppMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
